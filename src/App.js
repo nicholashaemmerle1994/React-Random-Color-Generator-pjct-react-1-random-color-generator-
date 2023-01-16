f0e558c0d88f8e5bf06b1a8e2e3d050c61387514
nicholashaemmerle1994 <nicholashaemmerle@gmx.at>
2023-01-16 19:34:21 +0100

    Change to some styles and alignment

--- src/App.js
+++ src/App.js
@@ -1,61 +1,59 @@
 import randomColor from 'randomcolor';
 import { useState } from 'react';
+import tachyons from 'tachyons';
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState(randomColor());
-  const [chosenHeight, setChosenHeight] = useState('');
-  const [chosenWidth, setChosenWidth] = useState('');
+  const [chosenHeight, setChosenHeight] = useState('100');
+  const [chosenWidth, setChosenWidth] = useState('100');
 
   return (
-    <>
-      <div>
-        <h1>Random Color Generator</h1>
-        <div
-          style={{
-            border: 'solid 5px',
-            height: `${chosenHeight}px`,
-            width: `${chosenWidth}px`,
-            backgroundColor: backgroundColor,
-            transition: 'all .5s ease',
-            WebkitTransition: 'all .5s ease',
-            MozTransition: 'all .5s ease',
-          }}
-        >
-          Generated Color: {backgroundColor}
-        </div>
-        <br />
-        <button
-          onClick={() => {
-            setBackgroundColor(randomColor());
-          }}
-        >
-          Generate
-        </button>
-        <br /> <br />
-        <label htmlFor="height">
-          Height:
-          <input
-            value={chosenHeight}
-            onChange={(event) => {
-              setChosenHeight(event.currentTarget.value);
-            }}
-            className="height"
-            type="text"
-          />
-        </label>
-        <br /> <br />
-        <label htmlFor="width">
-          Width:
-          <input
-            value={chosenWidth}
-            onChange={(event) => {
-              setChosenWidth(event.currentTarget.value);
-            }}
-            className="width"
-            type="text"
-          />
-        </label>
+    <div className="tc">
+      <h1>Random Color Generator</h1>
+      <div
+        className="dib"
+        style={{
+          border: 'solid 5px black',
+          height: `${chosenHeight}px`,
+          width: `${chosenWidth}px`,
+          backgroundColor: backgroundColor,
+          transition: 'all .5s ease',
+          WebkitTransition: 'all .5s ease',
+          MozTransition: 'all .5s ease',
+        }}
+      >
+        Generated Color: {backgroundColor}
       </div>
-    </>
+      <br /> <br />
+      <button
+        onClick={() => {
+          setBackgroundColor(randomColor());
+        }}
+      >
+        Generate
+      </button>
+      <br /> <br />
+      <label htmlFor="height">
+        Height:
+        <input
+          value={chosenHeight}
+          onChange={(event) => {
+            setChosenHeight(event.currentTarget.value);
+          }}
+          className="height"
+        />
+      </label>
+      <br /> <br />
+      <label htmlFor="width">
+        Width:
+        <input
+          value={chosenWidth}
+          onChange={(event) => {
+            setChosenWidth(event.currentTarget.value);
+          }}
+          className="width"
+        />
+      </label>
+    </div>
   );
 }
