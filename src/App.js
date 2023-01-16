c353271cc2307a806837f607b28922640ec85ada
nicholashaemmerle1994 <nicholashaemmerle@gmx.at>
2023-01-16 17:56:34 +0100

    Add smooth transition, width and hight

--- src/App.js
+++ src/App.js
@@ -2,29 +2,59 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 export default function App() {
-  const [backgroundColor, setBackgroundColor] = useState('');
-  console.log('outside', backgroundColor);
+  const [backgroundColor, setBackgroundColor] = useState(randomColor());
+  const [chosenHeight, setChosenHeight] = useState('');
+  const [chosenWidth, setChosenWidth] = useState('');
+
   return (
     <>
       <div>
         <h1>Random Color Generator</h1>
         <div
           style={{
-            height: '600px',
-            width: '600px',
+            border: 'solid 5px',
+            height: `${chosenHeight}px`,
+            width: `${chosenWidth}px`,
             backgroundColor: backgroundColor,
+            transition: 'all .5s ease',
+            WebkitTransition: 'all .5s ease',
+            MozTransition: 'all .5s ease',
           }}
         >
           Generated Color: {backgroundColor}
         </div>
+        <br />
         <button
           onClick={() => {
             setBackgroundColor(randomColor());
-            console.log('inside', backgroundColor);
           }}
         >
           Generate
         </button>
+        <br /> <br />
+        <label htmlFor="height">
+          Height:
+          <input
+            value={chosenHeight}
+            onChange={(event) => {
+              setChosenHeight(event.currentTarget.value);
+            }}
+            className="height"
+            type="text"
+          />
+        </label>
+        <br /> <br />
+        <label htmlFor="width">
+          Width:
+          <input
+            value={chosenWidth}
+            onChange={(event) => {
+              setChosenWidth(event.currentTarget.value);
+            }}
+            className="width"
+            type="text"
+          />
+        </label>
       </div>
     </>
   );
